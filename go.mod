e45da5192e1f482a7d58cc676eb082e54dbfb5ec
Christian Persson <Saser@users.noreply.github.com>
2022-01-01 15:19:52 +0000

    bazel: add Bazelisk as dependency and a script to invoke it.

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module go.saser.se
 
 go 1.17
+
+require github.com/bazelbuild/bazelisk v1.11.0
+
+require (
+	github.com/hashicorp/go-version v1.3.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+)
---
+++ tools.mk
@@ -0,0 +1,17 @@
+# tools.mk: rules for installing tools used by this project.
+
+root := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+
+# The underscore is to prevent the `go` command from considering any Go files that may exist in
+# downloaded third-party dependencies.
+tools := $(root)/_tools
+$(tools):
+	mkdir -p '$@'
+
+# bazelisk: a script to run Bazel with a given version.
+bazelisk := $(tools)/bazelisk
+$(bazelisk): go.mod $(tools)
+	go \
+		build \
+		-o='$@' \
+		github.com/bazelbuild/bazelisk
